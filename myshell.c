dc3ba80e1d9f66d7e513ba18d582232002b37d8b
dbids <dbids@bu.edu>
2020-09-10 19:08:28 -0400

    Working on the parse command section

--- myshell.c
+++ myshell.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h> //temp for strcat function
 
 #define TRUE 1
 
@@ -10,26 +9,41 @@ int main(int argc, char *argv[] )
 {
     while (TRUE)
     {
-        //PROMPT
+        /*.......................PROMPT.......................*/
         int writeBytes = write(1, "myshell$", 8);
         //printf("\nwrote with %d bytes\n", writeBytes);
 
-        //READ LINE
+        /*.......................READ.LINE.......................*/
         char line[32];
         int commandBytes = read(0, line, 8);
         printf(line);
 
-        //PARSE LINE
-            //Read the line to find the size of the parameters array
-            //Then define parameters and set each of the entries to one of the parameters, with the first being the command itself
-        char* parameters[3];
+        /*.......................PARSE.LINE.......................*/
+        //Read the line to find the snumber of space characters
+        char lineChar = line[0];
+        int space;
+        while (lineChar != '\0')
+            space += (lineChar == ' ') ? 1 : 0;
+
+        //Declar parameters array
+        char* parameters[space];
+        
+        //set each of the entries to one of the parameters, with the first being the command itself
+        lineChar = line[0];
+        for (int i = 0; i < space; i++)
+        {
+            while (lineChar != '\0')
+            {
+                
+            }
+        }
         parameters[0] = "ls";
         parameters[1] = ".";
         parameters[2] = NULL;
-        char command[40] = "/bin/";
-        strcat(command, line);
+        char command[40] = "/bin/ls";
+        printf(command);
 
-        //FORK
+        /*.......................FORK.......................*/
         int status;
         if (fork()!=0)
         {
