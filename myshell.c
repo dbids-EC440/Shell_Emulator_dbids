1eb9918612223dbcaab563054c60770d007d30af
dbids <dbids@bu.edu>
2020-09-13 19:46:34 -0400

    Encapsulated some code, and began working on getting the command parser to actually function

--- myshell.c
+++ myshell.c
@@ -10,13 +10,15 @@
 #define TOKEN_SIZE 32 //maximum length of tokens
 #define INPUT_SIZE 512 //maximum length of inputs
 
+char** getParameters(char* line, int space, bool firstIteration, char** parameters);
+char* getCommand(char** parameters, bool firstIteration);
+
 int main(int argc, char *argv[])  
 {
-    //Declare gdparameters initially
+    //Declare parameters initially
     char** parameters;
     bool firstIteration = true;
     bool endShell = false;
-    int space = 0;
 
     //Loop for shell
     while (TRUE)
@@ -53,52 +55,12 @@ int main(int argc, char *argv[])
         }
 
         /*.......................PARSE.LINE.......................*/
-        //Read the line to find the number of space characters
-        space = 0;
-        int lNum = 0;
-        while (line[lNum] != '\0')
-        {
-            space += (line[lNum] == ' ') ? 1 : 0;
-            lNum++;
-        }
-        
-        //Split up the line into different strings
-        char tempstr[space+2][TOKEN_SIZE];
-        lNum = 0;
-        for (i = 0; i < space+1; i++)
-        {
-            int tNum = 0;
-            while (line[lNum] != '\0' && line[lNum] != ' ' && line[lNum] != '\n')
-            {
-                tempstr[i][tNum] = line[lNum];
-                tNum++;
-                lNum++;
-            }
-            tempstr[i][tNum] = '\0';
-            lNum++;
-        }
-
-        //Declare parameters array dynamically and set equal to tempstr
-        int parameterSize = ((space+2) * sizeof(char*));
-        if (firstIteration)
-        {
-            parameters = malloc(parameterSize);
-            firstIteration = false;
-        }
-        else
-        {
-            parameters = realloc(parameters,parameterSize);
-        }
-        for (i = 0; i < space+1; i++)
-        {
-            parameters[i] = malloc(TOKEN_SIZE*sizeof(char));
-            parameters[i] = tempstr[i];
-        }
-        parameters[space+1] = NULL;
+        //Parse the parameters from the line
+        int space = 0;
+        char** parameters = getParameters(line, space, firstIteration, parameters);
 
         //Get command from the parameters
-        char command[TOKEN_SIZE] = "/bin/";
-        strcat(command, tempstr[0]);
+        char* command = getCommand(parameters, firstIteration);
 
         //Flush the output buffer for saftey
         fflush(stdout);
@@ -123,4 +85,89 @@ int main(int argc, char *argv[])
     {
         free(parameters);
     }   
+}
+
+
+char** getParameters(char* line, int space, bool firstIteration, char** parameters)
+{
+    //Read the line to find the number of space characters
+    space = 0;
+    int lNum = 0;
+    while (line[lNum] != '\0')
+    {
+        space += (line[lNum] == ' ') ? 1 : 0;
+        lNum++;
+    }
+
+    //Split up the line into different strings
+    char tempstr[space+2][TOKEN_SIZE];
+    lNum = 0;
+    int i;
+    for (i = 0; i < space+1; i++)
+    {
+        int tNum = 0;
+        while (line[lNum] != '\0' && line[lNum] != ' ' && line[lNum] != '\n')
+        {
+            tempstr[i][tNum] = line[lNum];
+            tNum++;
+            lNum++;
+        }
+        tempstr[i][tNum] = '\0';
+        lNum++;
+    }
+
+    //Declare parameters array dynamically and set equal to tempstr
+    int parameterSize = ((space+2) * sizeof(char*));
+    if (firstIteration)
+    {
+        parameters = malloc(parameterSize);
+        firstIteration = false;
+    }
+    else
+    {
+        parameters = realloc(parameters,parameterSize);
+    }
+    for (i = 0; i < space+1; i++)
+    {
+        parameters[i] = malloc(TOKEN_SIZE*sizeof(char));
+        parameters[i] = tempstr[i];
+    }
+    parameters[space+1] = NULL;
+
+    return parameters;
+}
+
+char* getCommand(char** parameters, bool firstIteration)
+{
+    int i;
+    long int li;
+    if (firstIteration)
+    {
+        //Get all the possible enviornment variable paths
+        const char* paths = getenv("PATH");
+        printf("path : %s\n", paths);
+        printf("length : %ld\n", strlen(paths));
+    
+        //Figure out the number of 
+        static int colon = 0;
+        for (li = 0; li < strlen(paths); li++)
+        {
+            colon += (paths[li] == ':') ? 1 : 0;
+            if (paths[li] == ':')
+            {
+                printf("%c,",paths[li]);
+                colon++;
+                printf("%d", colon);
+            }
+        }
+        printf("colon : %d\n", paths[li]);
+    }
+
+        char temp_command[TOKEN_SIZE] = "/bin/";
+        printf("%s\n", parameters[0]); //this is messed up
+        strcat(temp_command, parameters[0]);
+        char* command = temp_command;
+        printf("%s %s\n",temp_command, command);
+    
+    return command;
 }
