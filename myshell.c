14585f62a1fad029c1ac9d673119e08b99cae25f
dbids <dbids@bu.edu>
2020-09-13 18:40:32 -0400

    Initially seems like ctrl+D and command parsing have both been implemented correctly

--- myshell.c
+++ myshell.c
@@ -4,7 +4,9 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
+#define TRUE 1
 #define TOKEN_SIZE 32 //maximum length of tokens
 #define INPUT_SIZE 512 //maximum length of inputs
 
@@ -17,23 +19,36 @@ int main(int argc, char *argv[])
     int space = 0;
 
     //Loop for shell
-    while (!endShell)
+    while (TRUE)
     {
         /*.......................PROMPT.......................*/
         int writeBytes;
         if (argc == 1)
+        {
             writeBytes = write(1, "myshell$", 8);
+        }
         else if (argv[1] != "-n")
+        {
             write(3, "ERROR: command line argument not alowed", 39);
+        }
 
         /*.......................READ.LINE.......................*/
+        //Read the line
         char line[INPUT_SIZE];
         int commandBytes = read(0, line, INPUT_SIZE);
 
         //Check for ctrl+D input
-        if (line == "ctril+D")
+        int i;
+        /*while(line[i] != '\n')
         {
-            endShell = true;
+            if (line[i] == 4) 
+            {
+                endShell = true;
+            }
+            i++;
+        }*/
+        if (commandBytes == 0) 
+        {
             break;
         }
 
@@ -48,10 +63,9 @@ int main(int argc, char *argv[])
         }
         
         //Split up the line into different strings
-        char tempstr[space+1][TOKEN_SIZE];
-        int i;
+        char tempstr[space+2][TOKEN_SIZE];
         lNum = 0;
-        for (i = 0; i < space; i++)
+        for (i = 0; i < space+1; i++)
         {
             int tNum = 0;
             while (line[lNum] != '\0' && line[lNum] != ' ' && line[lNum] != '\n')
@@ -65,7 +79,7 @@ int main(int argc, char *argv[])
         }
 
         //Declare parameters array dynamically and set equal to tempstr
-        int parameterSize = ((space+1) * sizeof(char*));
+        int parameterSize = ((space+2) * sizeof(char*));
         if (firstIteration)
         {
             parameters = malloc(parameterSize);
@@ -83,9 +97,10 @@ int main(int argc, char *argv[])
         parameters[space+1] = NULL;
 
         //Get command from the parameters
-        char command[TOKEN_SIZE] = "/bin/ls";
+        char command[TOKEN_SIZE] = "/bin/";
+        strcat(command, tempstr[0]);
 
-        while ((getchar()) != '\n');
+        //Flush the output buffer for saftey
         fflush(stdout);
 
         /*.......................FORK.......................*/
@@ -94,7 +109,7 @@ int main(int argc, char *argv[])
         {
             //Parent Code
             waitpid(-1, &status, 0);
-            printf("The status was: %d", status);
+            printf("The status was: %d\n", status);
         }
         else
         {
@@ -104,5 +119,8 @@ int main(int argc, char *argv[])
     }
 
     //Free the dynamic memory
-    free(parameters);
+    if (!firstIteration)
+    {
+        free(parameters);
+    }   
 }
